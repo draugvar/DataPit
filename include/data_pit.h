7711ba8d6657200ccbe41ffea4ad0ec771bbd83c
Salvatore Rivieccio <salvatorerivieccio@duck.com>
2024-06-30 09:35:05 +0200

    unified consumer data

--- include/data_pit.h
+++ include/data_pit.h
@@ -31,7 +31,7 @@ class data_pit
 public:
     data_pit() : m_next_consumer_id(1)
     {
-        m_data_queues.clear();
+        m_queues_data.clear();
     }
     
     template<typename T>
@@ -39,7 +39,7 @@ public:
     {
         std::unique_lock global_lock(m_mtx);
         // check if the type is already registered and is the same
-        if (m_data_queues.contains(queue_id))
+        if (m_queues_data.contains(queue_id))
         {
             if(!get_queue(queue_id).empty() && get_type(queue_id) != std::type_index(typeid(T)).name())
             {
@@ -67,16 +67,16 @@ public:
     {
         std::unique_lock lock(m_mtx);
         // check if consumer_id exists
-        if (!m_consumer_indices.contains(consumer_id))
+        if (!m_consumers_data.contains(consumer_id))
         {
             lock.unlock();
             set_last_error(consumer_id, data_pit_error::consumer_not_found);
             return std::nullopt;
         }
 
-        auto queue_id = std::get<0>(m_consumer_indices.at(consumer_id));
+        auto queue_id = std::get<0>(m_consumers_data.at(consumer_id));
         // check if queue_id exists
-        if (m_data_queues.contains(queue_id))
+        if (m_queues_data.contains(queue_id))
         {
             if(get_type(queue_id) != std::type_index(typeid(T)).name())
             {
@@ -99,7 +99,7 @@ public:
             if(get_cv(queue_id).wait_for(queue_lock, std::chrono::milliseconds(timeout_ms),
                            [&]()
                            {
-                                return std::get<1>(m_consumer_indices.at(consumer_id)) < get_queue(queue_id).size();
+                                return std::get<1>(m_consumers_data.at(consumer_id)) < get_queue(queue_id).size();
                            }) == false)
             {
                 // unlock the mutex before returning
@@ -109,14 +109,14 @@ public:
                 return std::nullopt;
             }
         }
-        if (std::get<1>(m_consumer_indices.at(consumer_id)) >= get_queue(queue_id).size())
+        if (std::get<1>(m_consumers_data.at(consumer_id)) >= get_queue(queue_id).size())
         {
             queue_lock.unlock();
             set_last_error(consumer_id, data_pit_error::no_data_available);
             return std::nullopt;
         }
-        T data = std::any_cast<T>(get_queue(queue_id)[std::get<1>(m_consumer_indices.at(consumer_id))]);
-        std::get<1>(m_consumer_indices.at(consumer_id))++;
+        T data = std::any_cast<T>(get_queue(queue_id)[std::get<1>(m_consumers_data.at(consumer_id))]);
+        std::get<1>(m_consumers_data.at(consumer_id))++;
         return data;
     }
 
@@ -125,15 +125,14 @@ public:
         std::lock_guard lock(m_mtx);
         unsigned int consumer_id = register_id();
         if(consumer_id == 0) return 0;
-        m_consumer_indices[consumer_id] = std::make_tuple(queue_id, 0);
+        m_consumers_data[consumer_id] = std::make_tuple(queue_id, 0, data_pit_error::success);
         return consumer_id;
     }
 
     void unregister_consumer(unsigned int consumer_id)
     {
         std::lock_guard lock(m_mtx);
-        m_consumer_indices.erase(consumer_id);
-        m_consumer_last_error.erase(consumer_id);
+        m_consumers_data.erase(consumer_id);
         unregister_id(consumer_id);
     }
 
@@ -146,15 +145,15 @@ public:
     void clear_all_queues()
     {
         std::lock_guard lock(m_mtx);
-        m_data_queues.clear();
+        m_queues_data.clear();
     }
 
     void reset_consumer(unsigned int consumer_id)
     {
         std::lock_guard lock(m_mtx);
         // check if consumer_id exists
-        if (m_consumer_indices.find(consumer_id) == m_consumer_indices.end()) return;
-        std::get<1>(m_consumer_indices[consumer_id]) = 0;
+        if (m_consumers_data.find(consumer_id) == m_consumers_data.end()) return;
+        std::get<1>(m_consumers_data[consumer_id]) = 0;
     }
 
     void set_queue_size(int queue_id, size_t size)
@@ -168,11 +167,11 @@ public:
     {
         std::lock_guard lock(m_mtx);
         // check if consumer_id exists
-        if (m_consumer_indices.find(consumer_id) == m_consumer_indices.end())
+        if (m_consumers_data.find(consumer_id) == m_consumers_data.end())
         {
             return data_pit_error::consumer_not_found;
         }
-        return m_consumer_last_error[consumer_id];
+        return get_data_pit_error(consumer_id);
     }
 
 private:
@@ -182,41 +181,46 @@ private:
         std::lock_guard lock(m_mtx);
 
         // check if consumer_id exists
-        if (m_consumer_indices.find(consumer_id) == m_consumer_indices.end()) return;
+        if (m_consumers_data.find(consumer_id) == m_consumers_data.end()) return;
 
-        m_consumer_last_error[consumer_id] = error;
+        get_data_pit_error(consumer_id) = error;
     }
 
     inline void init_queue(int queue_id)
     {
-        auto&[vector, size] = std::get<0>(m_data_queues[queue_id]);
+        auto&[vector, size] = std::get<0>(m_queues_data[queue_id]);
         vector.clear();
         size = DATA_PIT_MAX_QUEUE_SIZE;
     }
 
     inline std::vector<std::any>& get_queue(int queue_id)
     {
-        return std::get<0>(m_data_queues.at(queue_id)).first;
+        return std::get<0>(m_queues_data.at(queue_id)).first;
     }
 
     inline size_t& get_queue_size(int queue_id)
     {
-        return std::get<0>(m_data_queues.at(queue_id)).second;
+        return std::get<0>(m_queues_data.at(queue_id)).second;
     }
 
     inline std::string& get_type(int queue_id)
     {
-        return std::get<1>(m_data_queues.at(queue_id));
+        return std::get<1>(m_queues_data.at(queue_id));
     }
 
     inline std::mutex& get_mutex(int queue_id)
     {
-        return std::get<2>(m_data_queues.at(queue_id));
+        return std::get<2>(m_queues_data.at(queue_id));
     }
 
     inline std::condition_variable& get_cv(int queue_id)
     {
-        return std::get<3>(m_data_queues.at(queue_id));
+        return std::get<3>(m_queues_data.at(queue_id));
+    }
+
+    inline data_pit_error& get_data_pit_error(unsigned int consumer_id)
+    {
+        return std::get<2>(m_consumers_data.at(consumer_id));
     }
 
     unsigned int register_id()
@@ -243,11 +247,14 @@ private:
         released_ids.push(consumer_id);
     }
 
-    typedef std::tuple<std::pair<std::vector<std::any>, size_t>, std::string, std::mutex, std::condition_variable> data;
+    typedef std::tuple<std::pair<std::vector<std::any>, size_t>, std::string, std::mutex, std::condition_variable> data_t;
+    typedef int queue_id_t;
+    typedef unsigned int consumer_id_t;
+    typedef size_t index_t;
+    typedef std::tuple<queue_id_t, index_t, data_pit_error> consumer_data_t;
 
-    std::unordered_map<int, data> m_data_queues;
-    std::unordered_map<unsigned int, std::tuple<int, size_t>> m_consumer_indices;
-    std::unordered_map<unsigned int, data_pit_error> m_consumer_last_error;
+    std::unordered_map<queue_id_t, data_t> m_queues_data;
+    std::unordered_map<consumer_id_t, consumer_data_t> m_consumers_data;
     std::recursive_mutex m_mtx;
     unsigned int m_next_consumer_id;
     std::queue<unsigned int> released_ids;
